450cf6058a4cef13cb3b2c4385795f18ea2ad9be
cartodev74 <cartodev@protonmail.com>
2026-08-20 10:33:30 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -15,13 +15,11 @@ conn = st.connection("postgresql", type="sql", ttl="10m")
 
 # --- Streamlit Config ---
 st.set_page_config(page_title="Fromage", layout="wide")
+st.title("📊 🧀CartoFromages v6 - Real-Time Live Fromage")
 
 # Perform query.
 df = conn.query('SELECT nom,id_inao FROM fromage;')
 
-
-## --- Tableau ---
-st.text(df)
 
 
 ## --- Select Box ---
@@ -32,20 +30,43 @@ sorted_df = df.sort_values(by='nom')
 # récupération du département sélectionné
 selected_fromage = st.selectbox(label='Choisissez une SIQO fromage', options= sorted_df['nom'],index=0)
 
+## --- Texte ---
+st.subheader("Texte")
+
+## --- Graph ---
+st.subheader("Graphique")
+
+## --- Map ---
+st.subheader("Carte interactive")
 
 
 
 
+# -- Sources ---
+st.subheader("Crédits")
+st.markdown("""
+<p>
+<em> Sources pour les données : </em>
+<ul>
+<li> Fond carte OpenSreetMap © OSM Contributors - IGN ADMIN EXPRESS Départements PE </li>
+<li> Données INAO SIQO Fromages 2025 - Licence ouverte ETALAB </li>
+</ul>
+</p>
+<p> 
+<em>  Sources pour la programmation : </em>
+<ul>
+<li> data-geek-lab / real time dashboard - MIT License - Copyright (c) 2025 Data-Geek-is-my-Name </li>
+<li> How to Build choropleth map in Python | Streamlit Tutorial #3 | Data Driven Maps With Python Folium - SCIENCE AND SCIENCE ONLY - Youtube</li>
+</ul>
+</p>
 
-# Print results.
-##for row in df.itertuples():
-##    st.write(f" nom : {row : nom} ")
+<br>
 
-
-
-
-
-# conn = st.connection("sql")
-#df = conn.query("SELECT * FROM fromage")
-# st.dataframe(df)
-
+<p>
+    <img src="https://www.cartodev.net/image/20240824_NIA_FR.png"> </img>
+    <br>
+    Réalisé sans intelligence articielle
+    <br>
+    <small> source logo © 2023 by Martine Peters (license creative commons BY-NC-SA 4.0) </small>
+</p>
+""", unsafe_allow_html=True)
