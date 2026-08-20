ff130842dada1438017f30d0879c7c6ec2a9929f
cartodev74 <cartodev@protonmail.com>
2026-08-20 09:46:27 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,47 @@
+import streamlit as st #création de la page web
+import sqlalchemy #récupération de sqlalchemy
+import psycopg2
+
+import os
+
+CUR_DIR = os.path.dirname(__file__)
+
+
+conn = st.connection("postgresql", type="sql")
+
+# --- Streamlit Config ---
+st.set_page_config(page_title="Fromage", layout="wide")
+
+# Perform query.
+df = conn.query('SELECT nom,id_inao FROM fromage;')
+
+
+## --- Tableau ---
+st.text(df)
+
+
+## --- Select Box ---
+st.subheader("Menu déroulant")
+
+sorted_df = df.sort_values(by='nom')
+
+# récupération du département sélectionné
+selected_fromage = st.selectbox(label='Choisissez une SIQO fromage', options= sorted_df['nom'],index=0)
+
+
+
+
+
+
+# Print results.
+##for row in df.itertuples():
+##    st.write(f" nom : {row : nom} ")
+
+
+
+
+
+# conn = st.connection("sql")
+#df = conn.query("SELECT * FROM fromage")
+# st.dataframe(df)
+
